9b482d6b8e432dfb702cc4b40500c0f03d027c31
ilya-dm <ilyadmitren@mail.ru>
2020-06-04 23:22:32 +0700

    удалил лишнее

--- pagination/app/views.py
+++ pagination/app/views.py
@@ -1,6 +1,4 @@
 import csv
-import pprint
-
 from django.core.paginator import Paginator
 from django.shortcuts import render_to_response, redirect, HttpResponse
 from django.urls import reverse
@@ -26,23 +24,8 @@ def bus_stations(request):
     if b_stations.has_previous():
         prev_page_url = b_stations.previous_page_number()
     return render_to_response('index.html', context={
-         'bus_stations': b_stations,
+        'bus_stations': b_stations,
         'current_page': current_page,
         'prev_page_url': f"{reverse(bus_stations)}?page={prev_page_url}",
         'next_page_url': f"{reverse(bus_stations)}?page={next_page_url}",
     })
-
-
-def pagi_view(request):
-    data = list()
-    with open('data-398-2018-08-30.csv') as csvfile:
-        reader = csv.DictReader(csvfile, delimiter=',')
-        for line in reader:
-            data.append(line)
-    page = int(request.GET.get('page'))
-    name = [i['Name'] for i in data]
-    street = [i['Street'] for i in data]
-    district = [i['District'] for i in data]
-    paginator = Paginator(name, 10)
-    msg = '<br/>'.join(paginator.get_page(page))
-    return HttpResponse(msg)
